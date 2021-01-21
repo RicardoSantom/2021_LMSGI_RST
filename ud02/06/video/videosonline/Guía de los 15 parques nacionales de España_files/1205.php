e22b171ecb9428fcf2dfd94a5cfa7d58e487d00a
RicardoSantom <richard1978_1@hotmail.com>
2021-01-21 10:04:03 +0100

    Prácticas del tema 2 y 3(solo rss y atom)

---
+++ ud02/06/video/videosonline/Guía de los 15 parques nacionales de España_files/1205.php
@@ -0,0 +1,13 @@
+var site=window.location.href;
+
+
+
+  var ddx_script = document.createElement("script"); 
+ddx_script.id = "ddx_script8"; 
+ddx_script.src = 'https://dmp.citiservi.es/pxtrack.js?mode=external&acc=85';
+ddx_script.async=true;
+document.body.appendChild(ddx_script);
+
+ 
+ 
+
